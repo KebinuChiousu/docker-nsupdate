bfcf5ddee18aa8aca6122e7f2716b02f1db52ea3
Kevin Meredith <kevin@meredithkm.info>
2018-06-20 20:54:06 -0400

    expose TCP port... and allow superuser config from ENV.

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -4,6 +4,6 @@
 
 python3 manage.py collectstatic
 python3 manage.py migrate
-python3 manage.py createsuperuser
+python3 manage.py createsuperuser --username $DJANGO_SUPERUSER --password $DJANGO_SUPERPASS
 
 uwsgi --uid=www-data --gid=www-data --ini uwsgi.ini
